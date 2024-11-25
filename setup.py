5389a9c650d31c7ae8106005b511a8513e5217aa
Developer 14 <developer14@OPENEYES.local>
2024-11-25 13:10:32 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='it's wafer using mlops',
+    author='dilipopen7',
+    license='MIT',
+)
